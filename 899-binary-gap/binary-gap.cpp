9c2f0fcfc86ad0ac6109c170f7eb483b74e87f5b
Abhishek-Tech-Cs <princesinghjan2019@gmail.com>
2026-02-22 18:56:33 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (94.66%) - LeetSync

---
+++ 899-binary-gap/binary-gap.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int index=-1,ans=INT_MIN;
+        int i=0;
+        while(n!=0){
+            if((n&1)==1 && index==-1) index=i;
+            else if((n&1)==1){
+                ans=max(ans,i-index);
+                index=i;
+            }
+            i++;
+            n=n>>1;
+        }
+        return ans==INT_MIN?0:ans;
+    }
+};
