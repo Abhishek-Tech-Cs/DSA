72a0d0f3d2ef51ef6ddda73f26030df521f72bf0
Abhishek-Tech-Cs <princesinghjan2019@gmail.com>
2026-03-21 19:42:55 +0530

    Time: 4 ms (15.81%) | Memory: 9.8 MB (77.34%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int st=0,end=s.size()-1;
+        while(st<end){
+            if(!isalnum(s[st])) st++;
+            else if(!isalnum(s[end])) end--;
+            else if(tolower(s[st])!=tolower(s[end])) return false;
+            else st++,end--;
+        }
+        return true;
+    }
+};
