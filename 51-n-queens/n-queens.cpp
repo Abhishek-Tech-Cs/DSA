3cce2218183344ffcaaef172b9be9411567f152f
Abhishek-Tech-Cs <princesinghjan2019@gmail.com>
2026-06-18 00:19:57 +0530

    Time: 11 ms (12.68%) | Memory: 19 MB (7.02%) - LeetSync

--- 51-n-queens/n-queens.cpp
+++ 51-n-queens/n-queens.cpp
@@ -1,47 +1,30 @@
 class Solution {
 public:
     vector<vector<string>> solveNQueens(int n) {
+        unordered_map<int,bool>col;
+        unordered_map<int,bool>diay;
+        unordered_map<int,bool>diax;
         vector<vector<string>>ans;
-        vector<vector<bool>>check(n,vector<bool>(n,true));
         vector<string>temp;
-        solve(ans,check,temp,n);
+        solve(n,ans,col,diay,diax,temp);
         return ans;
     }
-    void solve(vector<vector<string>> &ans,vector<vector<bool>> check,vector<string>temp,int n,int row=0){
+    void solve(int n, vector<vector<string>>&ans, unordered_map<int,bool>&checkCol, unordered_map<int,bool>&diay, unordered_map<int,bool>&diax, vector<string>temp, int row=0){
         if(row==n){
             ans.push_back(temp);
             return;
         }
 
         for(int col=0;col<n;col++){
-            if(check[row][col]){
-                vector<vector<bool>>tempCheck=check;
+            int y=row+col;  //       / -> for this type of diagonal (/);
+            int x=n-1+col-row; //    \ -> for this type of diagonal(\);
+            if(!checkCol[col] && !diay[y] && !diax[x]){
+                checkCol[col]=true,diay[y]=true,diax[x]=true;
                 string rowStr(n,'.');
                 rowStr[col]='Q';
                 temp.push_back(rowStr);
-                //row and col
-                for(int i=0;i<n;i++){
-                    tempCheck[row][i]=false;
-                    tempCheck[i][col]=false;
-                }
-                //diagonal
-                int tempRow = row;
-            for (int i = col; i < n && tempRow < n; i++) {
-                tempCheck[tempRow++][i] = false;
-            }
-            tempRow = row;
-            for (int i = col; i >= 0 && tempRow < n; i--) {
-                tempCheck[tempRow++][i] = false;
-            }
-            tempRow = row;
-            for (int i = col; i >= 0 && tempRow >= 0; i--) {
-                tempCheck[tempRow--][i] = false;
-            }
-            tempRow = row;
-            for (int i = col; i < n && tempRow >= 0; i++) {
-                tempCheck[tempRow--][i] = false;
-            }
-                solve(ans,tempCheck,temp,n,row+1);
+                solve(n,ans,checkCol,diay,diax,temp,row+1);
+                checkCol[col]=false,diay[y]=false,diax[x]=false;
                 temp.pop_back();
             }
         }
