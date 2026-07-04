01b26f1b862f2d07cf0d31e953e4c5ecff10309e
Abhishek-Tech-Cs <princesinghjan2019@gmail.com>
2026-07-04 10:02:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (81.33%) - LeetSync

--- 207-course-schedule/course-schedule.cpp
+++ 207-course-schedule/course-schedule.cpp
@@ -1,36 +1,39 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
-        if(prerequisites.size()==0) return true;
-        vector<unordered_map<int,bool>>graph(numCourses);
-        for(int i=0;i<prerequisites.size();i++){
-            int u=prerequisites[i][0];
-            int v=prerequisites[i][1];
-            graph[v][u]=true;
-        }
-        unordered_map<int,bool>vis;
-        unordered_map<int,bool>dfsCall;
-        for(int i=0;i<numCourses;i++){
-            if(!vis[i]){
-                bool check=detectCycle(i,vis,dfsCall,graph);
-                if(!check) return false;
-            }
-        }
-        return true;
+    vector<vector<int>> adj(numCourses);
+    vector<int> indegree(numCourses, 0);
+
+    // Build graph
+    for (auto &p : prerequisites) {
+        adj[p[1]].push_back(p[0]);
+        indegree[p[0]]++;
     }
-    bool detectCycle(int src,unordered_map<int,bool>&vis,unordered_map<int,bool>&dfsCall,vector<unordered_map<int,bool>>&graph){
-        vis[src]=true;
-        dfsCall[src]=true;
-        for(auto i:graph[src]){
-            if(!i.second) continue;
-            int val=i.first;
-            if(vis[val] && dfsCall[val]) return false;
-            else if(!vis[val]){
-                bool check=detectCycle(val,vis,dfsCall,graph);
-                if(!check) return false;
-            }
-        }
-        dfsCall[src]=false;
-        return true;
+
+    queue<int> q;
+
+    // Push all nodes with indegree 0
+    for (int i = 0; i < numCourses; i++) {
+        if (indegree[i] == 0)
+            q.push(i);
     }
+
+    int count = 0;
+
+    while (!q.empty()) {
+        int node = q.front();
+        q.pop();
+        count++;
+
+        for (int nbr : adj[node]) {
+            indegree[nbr]--;
+
+            if (indegree[nbr] == 0)
+                q.push(nbr);
+        }
+    }
+
+    // If all nodes were processed, no cycle exists
+    return count == numCourses;
+}
 };
