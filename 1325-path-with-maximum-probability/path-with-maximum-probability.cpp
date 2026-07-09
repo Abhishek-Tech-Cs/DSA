7ceff3e0395b899c8853f5e744760624f608e320
Abhishek-Tech-Cs <princesinghjan2019@gmail.com>
2026-07-09 15:29:14 +0530

    Time: 90 ms (6.83%) | Memory: 78.2 MB (5.01%) - LeetSync

---
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    double maxProbability(int n, vector<vector<int>>& edges, vector<double>& succProb, int start_node, int end_node) {
+
+        unordered_map<int,list<pair<int,double>>>adj;
+        int ptr=0;
+        for(auto i:edges){
+            int u=i[0];
+            int v=i[1];
+            double cost=succProb[ptr];
+            adj[u].push_back({v,cost});
+            adj[v].push_back({u,cost});
+            ptr++;
+        }
+
+        vector<int>vis(n,false);
+        priority_queue<pair<double,int>>q;
+        q.push({-1,start_node});            //cost node
+        vector<double>maxProb(n,0);
+        while(!q.empty()){
+            double cost=q.top().first;
+            int node=q.top().second;
+            q.pop();
+            for(auto i:adj[node]){
+                int ngbr=i.first;
+                double toNgbrCost=i.second;
+                double totalCost=toNgbrCost;
+                if(cost!=-1) totalCost=totalCost*cost;
+                if(totalCost>maxProb[ngbr]){
+                    maxProb[ngbr]=totalCost;
+                    q.push({totalCost,ngbr});
+                }
+            }
+        }
+        return maxProb[end_node];
+    }
+};
