440677292d503ba7d452ac68073ebe938ee818a1
Abhishek-Tech-Cs <princesinghjan2019@gmail.com>
2026-07-11 11:26:54 +0530

    Time: 66 ms (31.26%) | Memory: 150.6 MB (5.02%) - LeetSync

--- 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
+++ 2793-count-the-number-of-complete-components/count-the-number-of-complete-components.cpp
@@ -12,22 +12,36 @@ public:
         for(int i=0;i<n;i++){
             if(!vis[i]){
                 int V=0,E=0;
-                dfs(i,adj,vis,V,E);
+                queue<int>q;
+                q.push(i);
+                vis[i]=true;
+                while(!q.empty()){
+                    int node=q.front();
+                    q.pop();
+                    V++;
+                    for(auto &ngbr:adj[node]){
+                        E++;
+                        if(!vis[ngbr]){
+                            vis[ngbr]=true;
+                            q.push(ngbr);
+                        }
+                    }
+                }
                 if(V==1) ans++;
                 else if(V*(V-1)==E) ans++;
             }
         }
         return ans;
     }
-    void dfs(int &src,unordered_map<int,list<int>> &adj, vector<bool> &vis, int &V, int &E){
-        vis[src]=true;
-        V++;
-        for(auto &node:adj[src]){
-            E++;
-            if(!vis[node]){
-                vis[node]=true;
-                dfs(node,adj,vis,V,E);
-            }
-        }
-    }  
+    // void dfs(int &src,unordered_map<int,list<int>> &adj, vector<bool> &vis, int &V, int &E){
+    //     vis[src]=true;
+    //     V++;
+    //     for(auto &node:adj[src]){
+    //         E++;
+    //         if(!vis[node]){
+    //             vis[node]=true;
+    //             dfs(node,adj,vis,V,E);
+    //         }
+    //     }
+    // }  
 };
