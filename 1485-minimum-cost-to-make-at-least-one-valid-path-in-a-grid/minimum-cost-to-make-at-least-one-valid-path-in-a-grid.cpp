716dd45fcf2b2248ed1df52a280ae26fd533cde6
Abhishek-Tech-Cs <princesinghjan2019@gmail.com>
2026-07-15 12:35:15 +0530

    Time: 199 ms (5.48%) | Memory: 76.1 MB (5.05%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.cpp
@@ -1,44 +1,46 @@
-// *USING DIJKSTRA ALGO*
-
 using tup=tuple<int,int,int>;
 class Solution {
 public:
     int minCost(vector<vector<int>>& grid) {
         int m=grid.size(),n=grid[0].size();
         vector<vector<int>>cost(m,vector<int>(n,INT_MAX));
-        priority_queue<tup,vector<tup>,greater<tup>>pq;
+        deque<tup>dq;
         cost[0][0]=0;
-        pq.push({0,0,0});     //cost row col
-        while(!pq.empty()){
-            auto [blockCost,row,col]=pq.top();
-            pq.pop();
+        dq.push_front({0,0,0});     //cost row col
+        while(!dq.empty()){
+            auto [blockCost,row,col]=dq.front();
+            dq.pop_front();
             int costDown=blockCost,costUp=blockCost,costLeft=blockCost,costRight=blockCost;
             if(row>0){
                 if(grid[row][col]!=4) costUp++;
                 if(costUp<cost[row-1][col]){
                     cost[row-1][col]=costUp;
-                    pq.push({costUp,row-1,col});
+                    if(costUp==blockCost) dq.push_front({costUp,row-1,col});
+                    else dq.push_back({costUp,row-1,col});
                 }
             }
             if(row<m-1){
                 if(grid[row][col]!=3) costDown++;
                 if(costDown<cost[row+1][col]){
                     cost[row+1][col]=costDown;
-                    pq.push({costDown,row+1,col});
+                    if(costUp==blockCost) dq.push_front({costDown,row+1,col});
+                    else dq.push_back({costDown,row+1,col});
                 }
             }
             if(col>0){
                 if(grid[row][col]!=2) costLeft++;
                 if(costLeft<cost[row][col-1]){
                     cost[row][col-1]=costLeft;
-                    pq.push({costLeft,row,col-1});
+                    if(costUp==blockCost) dq.push_front({costLeft,row,col-1});
+                    else dq.push_back({costLeft,row,col-1});
                 }
             }
             if(col<n-1){
                 if(grid[row][col]!=1) costRight++;
                 if(costRight<cost[row][col+1]){
                     cost[row][col+1]=costRight;
-                    pq.push({costRight,row,col+1});
+                    if(costUp==blockCost) dq.push_front({costRight,row,col+1});
+                    else dq.push_back({costRight,row,col+1});
                 }
             }
         }
