ecd4923233eb3ac08371451b5ca8c9419bdc376d
Abhishek-Tech-Cs <princesinghjan2019@gmail.com>
2026-07-16 11:54:34 +0530

    Time: 63 ms (93.40%) | Memory: 148.7 MB (99.08%) - LeetSync

--- 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
+++ 4242-sum-of-gcd-of-formed-pairs/sum-of-gcd-of-formed-pairs.cpp
@@ -4,21 +4,21 @@ public:
         int mxi=INT_MIN;
         for(int i=0;i<nums.size();i++){
             mxi=max(mxi,nums[i]);
-            int gcdVal=gcd(mxi,nums[i]);
+            int gcdVal=findGCD(mxi,nums[i]);
             nums[i]=gcdVal;
         }
         sort(nums.begin(),nums.end());
         int smallest=0,largest=nums.size()-1;
         long long ans=0;
         while(smallest<largest){
-            ans+=gcd(nums[smallest],nums[largest]);
+            ans+=findGCD(nums[smallest],nums[largest]);
             smallest++,largest--;
         }
         return ans;
     }
-    int gcd(int a,int b){
+    int findGCD(int a,int b){
         if(a==0) return b;
         if(b==0) return a;
-        return gcd(b,a%b);
+        return findGCD(b,a%b);
     }
 };
