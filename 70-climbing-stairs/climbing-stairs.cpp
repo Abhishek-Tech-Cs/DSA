fa863e768ca272e631930992b1dadeb78955043b
Abhishek Singh <princesinghjan2019@gmail.com>
2026-08-09 15:02:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (39.16%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int>memo(n+1,-1);
+        return dfs(n,memo);
+    }
+    int dfs(int n, vector<int> &memo){
+        if(n==1 || n==0) return 1;
+
+        if(memo[n]!=-1) return memo[n];
+
+        memo[n]=dfs(n-1,memo)+dfs(n-2,memo);
+
+        return memo[n];
+    }
+};
