d767f0edd617f4dcaf3a99f9695a9a27bfabd596
Abhishek Singh <princesinghjan2019@gmail.com>
2026-08-14 10:09:22 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (24.20%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    int t[101];
     int rob(vector<int>& nums) {
-        memset(t, -1, sizeof(t));
-        return dfs(nums);
-    }
-    int dfs(vector<int>& nums,int i=0){
-        if(i>=nums.size()) return 0;
+        vector<int>t(nums.size()+1);
+        t[0]=0;
+        t[1]=nums[0];
+        for(int i=2;i<nums.size()+1;i++){
+            int steal=nums[i-1]+t[i-2];
+            int skip=t[i-1];
 
-        if(t[i]!=-1) return t[i];
-        int steal = nums[i]+dfs(nums,i+2);
-        int skip = dfs(nums,i+1);
-
-        return t[i] = max(steal,skip);
+            t[i]=max(steal,skip);
+        }
+        return t[nums.size()];
     }
 };
