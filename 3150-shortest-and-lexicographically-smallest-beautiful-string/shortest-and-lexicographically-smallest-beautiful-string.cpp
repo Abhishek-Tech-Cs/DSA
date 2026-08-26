46a5cea45fc44a03c73ca9efefe90b7190241103
Abhishek Singh <princesinghjan2019@gmail.com>
2026-08-26 12:26:14 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (64.74%) - LeetSync

--- 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
+++ 3150-shortest-and-lexicographically-smallest-beautiful-string/shortest-and-lexicographically-smallest-beautiful-string.cpp
@@ -7,21 +7,19 @@ public:
         int one=0;
         for(int right=0;right<s.size();right++){
             if(s[right]=='1') one++;
-            temp.push_back(s[right]);
             if(one<k) continue;
-            while(left < right && one > k){
+            
+            while(one > k) {
                 if(s[left]=='1') one--;
-                temp.erase(0,1);
                 left++;
-            }
-            while(!temp.empty() && temp[0]=='0'){
-                left++;
-                temp.erase(0,1);
             }
 
-            if(ans.empty()) ans=temp;
-            else if(ans.size()>temp.size()) ans=temp;
-            else if(ans.size()==temp.size() && ans>temp) ans=temp;
+            while(left < right && s[left] == '0' ) left++;
+
+            string curr = s.substr(left, right - left + 1);
+
+            if( ans == "" || curr.size() < ans.size() || (curr.size() == ans.size() && curr < ans)) 
+                ans = curr;
         }
         return ans;
     }
