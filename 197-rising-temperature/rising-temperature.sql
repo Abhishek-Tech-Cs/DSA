f72521dc0675869e8bab5dc59f7d51ea680344dc
Abhishek Singh <princesinghjan2019@gmail.com>
2026-09-26 12:54:39 +0530

    Time: 270 ms (38.34%) | Memory: 0B (100.00%) - LeetSync

--- 197-rising-temperature/rising-temperature.sql
+++ 197-rising-temperature/rising-temperature.sql
@@ -1,4 +1,3 @@
 -- Write your PostgreSQL query statement below
 select today.id from weather today
-join weather yesterday on today.recordDate - yesterday.recordDate = 1
-where today.temperature > yesterday.temperature
+join weather yesterday on today.recordDate - yesterday.recordDate = 1 and today.temperature > yesterday.temperature
